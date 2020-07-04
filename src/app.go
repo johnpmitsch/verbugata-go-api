9bbe85be4bb8d87e2aab2254155827ebe7a3acf0
John Mitsch <jomitsch@redhat.com>
2020-07-04 14:37:50 -0400

    Added chi router

--- src/app.go
+++ src/app.go
@@ -7,21 +7,19 @@ import (
 	"time"
 
 	"github.com/johnpmitsch/verbugata-go-api/config"
+	"github.com/johnpmitsch/verbugata-go-api/router"
 )
 
 func main() {
 	appConf := config.AppConfig()
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", Greet)
-
+	appRouter := router.New()
 	address := fmt.Sprintf(":%d", appConf.Server.Port)
 
 	log.Printf("Starting server %s\n", address)
 
 	s := &http.Server{
 		Addr:         address,
-		Handler:      mux,
+		Handler:      appRouter,
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second,
 		IdleTimeout:  120 * time.Second,
@@ -31,6 +29,7 @@ func main() {
 	}
 }
 
+// Greets user
 func Greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,7 @@ module github.com/johnpmitsch/verbugata-go-api
 
 go 1.14
 
-require github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
+)
